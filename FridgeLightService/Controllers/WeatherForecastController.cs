dcc928816473686dfb71f68f19ca283a289a49ee
Tymoniden <timo.steib@gmail.com>
2021-07-02 23:21:45 +0200

    dummy change

--- FridgeLightService/Controllers/WeatherForecastController.cs
+++ FridgeLightService/Controllers/WeatherForecastController.cs
@@ -31,7 +31,7 @@ namespace FridgeLightService.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)]+"_"
             })
             .ToArray();
         }
